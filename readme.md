c4072996cbe460ffcadacf62a957a130eb562888
Damian Brady <dev@damianbrady.com.au>
2015-05-20 15:05:44 +1000

    Public version of Build.Preview custom task for Octopus

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Welcome!
+
+OctoTFS is a repository containing components for integration with Team Foundation Server and Visual Studio Online.
+
+Currently, it contains the following:
+* Custom Build Tasks for [Team Build vNext](http://vsalmdocs.azurewebsites.net/library/vs/alm/build/overview)
+
+... yeah, that's it right now.
---
+++ source/CreateOctopusRelease/Octopus-CreateRelease.ps1
@@ -0,0 +1,132 @@
+﻿param(
+	[string] [Parameter(Mandatory = $true)]
+	$ConnectedServiceName,
+	[string] [Parameter(Mandatory = $true)]
+	$ProjectName,
+	[string] [Parameter(Mandatory = $false)]
+	$ChangesetCommentReleaseNotes,
+	[string] [Parameter(Mandatory = $false)]
+	$WorkItemReleaseNotes,
+	[string] [Parameter(Mandatory = $false)]
+	$DeployTo,
+	[string] [Parameter(Mandatory = $false)]
+	$AdditionalArguments
+)
+
+#todo:
+#  - handle non-TFVC repositories
+
+Write-Verbose "Entering script Octopus-CreateRelease.ps1"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+
+# Get release notes from linked changesets and work items
+function Get-LinkedReleaseNotes($vssEndpoint, $comments, $workItems) {
+	$personalAccessToken = $vssEndpoint.Authorization.Parameters.AccessToken
+	
+	$changesUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$($env:SYSTEM_TEAMPROJECTID)/_apis/build/builds/$($env:BUILD_BUILDID)/changes"
+	$headers = @{Authorization = "Bearer $personalAccessToken"}
+	$relatedChanges = (Invoke-WebRequest -Uri $changesUri -Headers $headers) | ConvertFrom-Json
+	Write-Host "Related Changes = $relatedChanges"
+	
+	$releaseNotes = ""
+	$nl = "`r`n`r`n"
+	if ($comments -eq $true) {
+		Write-Host "Adding changeset comments to release notes"
+		$releaseNotes += "**Changeset Comments:**$nl"
+		$relatedChanges.value | ForEach-Object {$releaseNotes += "* [$($_.id) - $($_.author.displayName)]($(ChangesetUrl $_.location)): $($_.message)$nl"}
+	}
+	
+	if ($workItems -eq $true) {
+		Write-Host "Adding work items to release notes"
+		$releaseNotes += "**Work Items:**$nl"
+		foreach ($c in $relatedChanges.value) {
+			# work item id is a hack because id is prefixed with "C", and I'm not 100% sure it's consistent.
+			$wiId = $c.location.Substring($c.location.LastIndexOf("/")+1)
+			$wiUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)/_apis/tfvc/changesets/$wiId/workItems"
+			$wi = (Invoke-WebRequest -Uri $wiUri -Headers $headers) | ConvertFrom-Json
+			$wi.value | ForEach-Object {$releaseNotes += "* [$($_.id)]($($_.webUrl)): $($_.title) [$($_.state)]$nl"}
+		}
+	}
+	
+	return $releaseNotes
+}
+function ChangesetUrl($apiUrl) {
+	$wiId = $apiUrl.Substring($apiUrl.LastIndexOf("/")+1)
+	return "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$($env:SYSTEM_TEAMPROJECTID)/_versionControl/changeset/$wiId"
+}
+
+
+# Returns the Octo.exe parameters for credentials
+function Get-OctoCredentialParameters($serviceDetails) {
+	$pwd = $serviceDetails.Authorization.Parameters.Password
+	if ($pwd.StartsWith("API-")) {
+        return "--apiKey=$pwd"
+    } else {
+        $un = $serviceDetails.Authorization.Parameters.Username
+        return "--user=`"$un`" --pass=`"$pass`""
+    }
+}
+
+
+
+# Returns a path to the Octo.exe file
+function Get-PathToOctoExe() {
+	$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.ScriptBlock.File
+	$targetPath = Join-Path -Path $PSScriptRoot -ChildPath "Octo.exe" 
+	return $targetPath
+}
+
+
+
+# Create a Release Notes file for Octopus
+function Create-ReleaseNotes($linkedItemReleaseNotes) {
+	$buildNumber = $env:BUILD_BUILDNUMBER #works
+	$buildId = $env:BUILD_BUILDID #works
+	$projectName = $env:SYSTEM_TEAMPROJECT	#works
+	#$buildUri = $env:BUILD_BUILDURI #works but is a vstfs:/// link
+	#Note: This URL will undoubtedly change in the future
+	$buildUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$projectName/_BuildvNext#_a=summary&buildId=$buildId"
+	$buildName = $env:BUILD_DEFINITIONNAME	#works
+	$repoName = $env:BUILD_REPOSITORY_NAME	#works
+	#$repoUri = $env:BUILD_REPOSITORY_URI #nope :(
+	$notes = "Release created by Build [${buildName} #${buildNumber}](${buildUri}) in Project ${projectName} from the ${repoName} repository."
+	if (-not [System.String]::IsNullOrWhiteSpace($linkedItemReleaseNotes)) {
+		$notes += "`r`n`r`n$linkedItemReleaseNotes"
+	}
+	$fileguid = [guid]::NewGuid()
+	$fileLocation = Join-Path -Path $env:BUILD_STAGINGDIRECTORY -ChildPath "release-notes-$fileguid.md"
+	$notes | Out-File $fileLocation
+	
+	return "--releaseNotesFile=`"$fileLocation`""
+}
+
+
+### Execution starts here ###
+
+# Get required parameters
+$connectedServiceDetails = Get-ServiceEndpoint -Name "$ConnectedServiceName" -Context $distributedTaskContext 
+$credentialParams = Get-OctoCredentialParameters($connectedServiceDetails)
+$octopusUrl = $connectedServiceDetails.Url
+
+# Get release notes
+$linkedReleaseNotes = ""
+$wiReleaseNotes = Convert-String $WorkItemReleaseNotes Boolean
+$commentReleaseNotes = Convert-String $ChangesetCommentReleaseNotes Boolean
+if ($wiReleaseNotes -or $commentReleaseNotes) {
+	$vssEndPoint = Get-ServiceEndPoint -Name "SystemVssConnection" -Context $distributedTaskContext
+	$linkedReleaseNotes = Get-LinkedReleaseNotes $vssEndPoint $commentReleaseNotes $wiReleaseNotes
+}
+$releaseNotesParam = Create-ReleaseNotes $linkedReleaseNotes
+
+#deployment arguments
+if (-not [System.String]::IsNullOrWhiteSpace($DeployTo)) {
+	$deployToParams = "--deployTo=`"$DeployTo`""
+}
+
+# Call Octo.exe
+$octoPath = Get-PathToOctoExe
+Write-Output "Path to Octo.exe = $octoPath"
+Invoke-Tool -Path $octoPath -Arguments "create-release --project=`"$ProjectName`" --server=$octopusUrl $credentialParams $deployToParams $releaseNotesParam $AdditionalArguments"
+
+Write-Verbose "Finishing Octopus-CreateRelease.ps1"
