2bbaa1cbb710d1d1d0dcf2a06e28af8eaeeaf4bf
Damian Brady <dev@damianbrady.com.au>
2015-11-27 14:04:48 +1000

    Fixed JSON parsing missed in last merge

--- source/VSTSExtensions/OctopusBuildTasks/CreateOctopusRelease/Octopus-CreateRelease.ps1
+++ source/VSTSExtensions/OctopusBuildTasks/CreateOctopusRelease/Octopus-CreateRelease.ps1
@@ -51,13 +51,15 @@ function Get-LinkedReleaseNotes($vssEndpoint, $comments, $workItems) {
 
 		$relatedWorkItemsUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$($env:SYSTEM_TEAMPROJECTID)/_apis/build/builds/$($env:BUILD_BUILDID)/workitems?api-version=2.0"
 		Write-Host "Performing POST request to $relatedWorkItemsUri"
-		$relatedWorkItems = (Invoke-WebRequest -Uri $relatedWorkItemsUri -Method POST -Headers $headers -UseBasicParsing -ContentType "application/json") | ConvertFrom-Json
+		$relatedWiResponse = Invoke-WebRequest -Uri $relatedWorkItemsUri -Method POST -Headers $headers -UseBasicParsing -ContentType "application/json"
+-		$relatedWorkItems = $relatedWiResponse.Content | ConvertFrom-Json
 		
 		Write-Host "Retrieved $($relatedWorkItems.count) work items"
 		if ($relatedWorkItems.count -gt 0) {
 			$workItemsUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)/_apis/wit/workItems?ids=$(($relatedWorkItems.value.id) -join '%2C')"
 			Write-Host "Performing GET request to $workItemsUri"
-			$workItemsDetails = (Invoke-WebRequest -Uri $workItemsUri -Headers $headers -UseBasicParsing) | ConvertFrom-Json
+			$relatedWiDetailsResponse = Invoke-WebRequest -Uri $workItemsUri -Headers $headers -UseBasicParsing
+-			$workItemsDetails = $relatedWiDetailsResponse.Content | ConvertFrom-Json
 		
 			$workItemEditBaseUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$($env:SYSTEM_TEAMPROJECTID)/_workitems/edit"
 			$workItemsDetails.value | ForEach-Object {$releaseNotes += "* [$($_.id)]($workItemEditBaseUri/$($_.id)): $($_.fields.'System.Title') [$($_.fields.'System.State')] $(GetWorkItemTags($_.fields)) $nl"}
