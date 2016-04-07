964967aa385523616739a17301c85e15494cddb7
Michael Noonan <michaelnoonan@gmail.com>
2016-04-07 14:08:21 +1000

    Publish VSIX as Octopus Artifacts.

--- deploy.ps1
+++ deploy.ps1
@@ -6,3 +6,9 @@ $accessToken = $OctopusParameters["AccessToken"]
 
 & "$PSScriptRoot\pack.ps1" -environment $environment -version $version
 & "$PSScriptRoot\publish.ps1" -environment $environment -version $version -accessToken $accessToken
+
+$vsixPackages = Get-ChildItem "$PSScriptRoot\build\Artifacts\$environment\*.vsix"
+
+foreach ($vsix in $vsixPackages) {
+    New-OctopusArtifact -Path $vsix    
+}
