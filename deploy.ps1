47d75a4114e0d49ee3cb70959fcad2eeca00171c
Michael Richardson <michael.james.richardson@gmail.com>
2016-04-18 18:17:12 +1000

    Added ShareWith parameter supplied from Octopus variable

--- deploy.ps1
+++ deploy.ps1
@@ -3,6 +3,8 @@ $ErrorActionPreference = "Stop"
 $environment = $OctopusParameters["Octopus.Environment.Name"]
 $version = $OctopusParameters["Octopus.Release.Number"]
 $accessToken = $OctopusParameters["AccessToken"]
+$shareWith = $OctopusParameters["ShareWith"]
+
 
 & "$PSScriptRoot\pack.ps1" -environment $environment -version $version
 & "$PSScriptRoot\publish.ps1" -environment $environment -version $version -accessToken $accessToken
