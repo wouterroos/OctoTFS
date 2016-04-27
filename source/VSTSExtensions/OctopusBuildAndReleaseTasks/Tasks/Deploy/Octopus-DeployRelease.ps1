0d13fa73b60d5c35d9ad61381f21baad9b5663ab
Michael Richardson <michael.james.richardson@gmail.com>
2016-04-27 17:39:19 +1000

    Octopus-DeployRelease.ps1 fixes

--- source/VSTSExtensions/OctopusBuildAndReleaseTasks/Tasks/Deploy/Octopus-DeployRelease.ps1
+++ source/VSTSExtensions/OctopusBuildAndReleaseTasks/Tasks/Deploy/Octopus-DeployRelease.ps1
@@ -7,14 +7,25 @@ try {
 
     . .\Octopus-VSTS.ps1
 
+    $ConnectedServiceName = Get-VstsInput -Name ConnectedServiceName -Require
+    $Project = Get-VstsInput -Name Project -Require
+    $ReleaseNumber = Get-VstsInput -Name ReleaseNumber -Require
+    $Environments = Get-VstsInput -Name Environments -Require
+    $ShowProgress = Get-VstsInput -Name ShowProgress -AsBool
+
     $connectedServiceDetails = Get-VstsEndpoint -Name "$ConnectedServiceName" -Require
     $credentialArgs = Get-OctoCredentialArgs($connectedServiceDetails)
     $octopusUrl = $connectedServiceDetails.Url
 
     # Call Octo.exe
     $octoPath = Get-OctoExePath
-    Write-Output "Path to Octo.exe = $octoPath"
-    $Arguments = "deploy-release --project=`"$Project`" --server=$octopusUrl $credentialArgs $AdditionalArguments"
+    $Arguments = "deploy-release --project=`"$Project`" --releaseNumber=`"$ReleaseNumber`" --progress="$ShowProgress" --server=$octopusUrl $credentialArgs $AdditionalArguments"
+    if ($Environments) {
+        ForEach($Environment in $Environments.Split(',').Trim()) {
+            $Arguments = $Arguments + " --deployto=`"$Environment`""
+        }
+    }
+
     Invoke-VstsTool -FileName $octoPath -Arguments $Arguments
 
 } finally {
