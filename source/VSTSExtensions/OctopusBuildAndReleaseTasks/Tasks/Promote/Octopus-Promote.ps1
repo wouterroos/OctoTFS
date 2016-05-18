60ab60cf58bdc5a3ce3e21bc5501c7f1f8a7bfcc
Michael Richardson <michael.james.richardson@gmail.com>
2016-05-18 14:15:52 +1000

    Added promote task

---
+++ source/VSTSExtensions/OctopusBuildAndReleaseTasks/Tasks/Promote/Octopus-Promote.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding()]
+param()
+
+Trace-VstsEnteringInvocation $MyInvocation
+
+try {
+
+    . .\Octopus-VSTS.ps1
+
+    $ConnectedServiceName = Get-VstsInput -Name ConnectedServiceName -Require
+    $Project = Get-VstsInput -Name Project -Require
+    $From = Get-VstsInput -Name From -Require
+    $To = Get-VstsInput -Name To -Require
+    $ShowProgress = Get-VstsInput -Name ShowProgress -AsBool
+    $AdditionalArguments = Get-VstsInput -Name AdditionalArguments
+
+    $connectedServiceDetails = Get-VstsEndpoint -Name "$ConnectedServiceName" -Require
+    $credentialArgs = Get-OctoCredentialArgs($connectedServiceDetails)
+    $octopusUrl = $connectedServiceDetails.Url
+
+    # Call Octo.exe
+    $octoPath = Get-OctoExePath
+    $Arguments = "promote-release --project=`"$Project`" --from=`"$From`" --progress=$ShowProgress --server=$octopusUrl $credentialArgs $AdditionalArguments"
+    if ($To) {
+        ForEach($Environment in $To.Split(',').Trim()) {
+            $Arguments = $Arguments + " --to=`"$Environment`""
+        }
+    }
+    Invoke-VstsTool -FileName $octoPath -Arguments $Arguments -RequireExitCodeZero
+
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
