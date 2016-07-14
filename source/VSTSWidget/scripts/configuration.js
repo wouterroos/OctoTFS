51e0d1cbf31f0ee61fdbf2058280b46ccdc7c5c4
Wouter Roos <wrmmroos@gmail.com>
2016-07-14 23:52:48 +0200

    - implemented first working (Development only) test version

---
+++ source/VSTSWidget/scripts/configuration.js
@@ -0,0 +1,67 @@
+﻿var baseUrl = "https://octopus.bdo.global";
+function DoRequest(requestUrl, successCallback) {
+    $.ajax({
+        type: "GET",
+        url: baseUrl + requestUrl,
+        beforeSend: function (request) {
+            request.setRequestHeader("X-Octopus-ApiKey", "API-ZK8Q0TCGQLSCOZZN1DJHGPLHPFI")
+        },
+        success: successCallback
+    });
+}
+
+VSS.init({
+    explicitNotifyLoaded: true,
+    usePlatformStyles: true
+});
+
+VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
+    WidgetHelpers.IncludeWidgetConfigurationStyles();
+    VSS.register("OctopusWidget.Configuration", function () {
+
+        var projectsDropdown = $("#project-dropdown");
+
+        return {
+            load: function (widgetSettings, widgetConfigurationContext) {
+                var settings = JSON.parse(widgetSettings.customSettings.data);
+
+                DoRequest("/api/projects", function(getProjectsResult) {
+                    if (getProjectsResult && getProjectsResult.Items) {
+
+                        for (var i = 0; i < getProjectsResult.Items.length; i++) {
+                            var project = getProjectsResult.Items[i];
+                            projectsDropdown.append($("<option></option>").attr("value", project.Id).text(project.Name))
+                        }
+
+                        if (settings && settings.projectId) {
+                            projectsDropdown.val(settings.projectId);
+                        }
+                    }
+
+                    return WidgetHelpers.WidgetStatusHelper.Success();
+                });
+
+                projectsDropdown.on("change", function () {
+                    var customSettings = {
+                        data: JSON.stringify({
+                            projectId: projectsDropdown.val()
+                        })
+                    };
+
+                    var eventName = WidgetHelpers.WidgetEvent.ConfigurationChange;
+                    var eventArgs = WidgetHelpers.WidgetEvent.Args(customSettings);
+                    widgetConfigurationContext.notify(eventName, eventArgs);
+                })
+            },
+            onSave: function () {
+                var customSettings = {
+                    data: JSON.stringify({
+                        projectId: projectsDropdown.val()
+                    })
+                };
+                return WidgetHelpers.WidgetConfigurationSave.Valid(customSettings);
+            }
+        }
+    });
+    VSS.notifyLoadSucceeded();
+});
--- source/VSTSWidget/scripts/octopus.js
+++ source/VSTSWidget/scripts/octopus.js
@@ -1,14 +1,16 @@
 ﻿var baseUrl = "https://octopus.bdo.global";
 
-var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function () {
-
+function DoRequest(requestUrl) {
+    return $.ajax({
+        type: "GET",
+        url: baseUrl + requestUrl,
+        beforeSend: function (request) {
+            request.setRequestHeader("X-Octopus-ApiKey", "API-ZK8Q0TCGQLSCOZZN1DJHGPLHPFI")
+        }
+    });
 }
 
-xhttp.open("GET", baseUrl + "/api/environments/all");
-xhttp.setRequestHeader("X-Octopus-ApiKey", "API-ZK8Q0TCGQLSCOZZN1DJHGPLHPFI");
-xhttp.send();
 
 VSS.init({
     explicitNotifyLoaded: true,
@@ -18,13 +20,94 @@ VSS.init({
 VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
     WidgetHelpers.IncludeWidgetStyles();
     VSS.register("OctopusWidget", function () {
+
+        var getCurrentDeploymentStatus = function GetCurrentDeploymentStatus(widgetSettings) {
+            var settings = JSON.parse(widgetSettings.customSettings.data);
+            var project = null;
+            var environment = null;
+            var lastDeployment = null;
+            var release = null;
+            var task = null;
+
+            if (settings && settings.projectId) {
+                var projectId = settings.projectId;
+                var environmentName = "Development";
+
+                $("#environmentName").text(environmentName);
+
+                $.when
+                (
+                    DoRequest("/api/projects/" + projectId),
+                    DoRequest("/api/environments")
+                )
+                .done(function (getProjectResult, getEnvironmentsResult) {
+
+                    var project = getProjectResult[0];
+                    var environment = null;
+
+                    if (getEnvironmentsResult && getEnvironmentsResult[0] && getEnvironmentsResult[0].Items) {
+                        var environments = getEnvironmentsResult[0].Items
+                        for (var j = 0; j < environments.length; j++) {
+                            if (environments[j].Name.toLowerCase() === environmentName.toLowerCase()) {
+                                environment = environments[j];
+                                break;
+                            }
+                        }
+                    }
+
+                    var lastDeployment = null;
+                    var release = null;
+                    var task = null;
+
+                    $.when
+                    (
+                        DoRequest("/api/deployments?projects=" + project.Id + "&environments=" + environment.Id)
+                    )
+                    .done(function (getDeploymentsResult) {
+
+                        if (getDeploymentsResult && getDeploymentsResult.Items) {
+                            lastDeployment = getDeploymentsResult.Items[0];
+
+                            $.when
+                            (
+                                DoRequest(lastDeployment.Links.Release),
+                                DoRequest(lastDeployment.Links.Task
+                                )
+                            )
+                            .done(function (getReleaseResult, getTaskResult) {
+                                release = getReleaseResult[0];
+                                task = getTaskResult[0];
+
+                                if (task.State === "Success") {
+                                    $(".widget").css("background-color", "#339933");
+                                    $("#statusIcon").addClass("fa-check");
+                                }
+                                if (task.State === "Failed") {
+                                    $(".widget").css("background-color", "#e60017");
+                                    $("#statusIcon").addClass("fa-exclamation-triangle");
+                                }
+                                if (task.State === "Executing") {
+                                    $(".widget").css("background-color", "#009ccc");
+                                    $("#statusIcon").addClass("fa-spinner fa-spin");
+                                }
+
+                                $("#version").text(release.Version)
+                                return WidgetHelpers.WidgetStatusHelper.Success();
+                            });
+                        }
+                    });
+                });
+            }
+
+            return WidgetHelpers.WidgetStatusHelper.Success();
+        }
+
         return {
             load: function (widgetSettings) {
-
-                var $title = $('h2.title');
-                $title.text('Hello World');
-
-                return WidgetHelpers.WidgetStatusHelper.Success();
+                return getCurrentDeploymentStatus(widgetSettings);
+            },
+            reload: function (widgetSettings) {
+                return getCurrentDeploymentStatus(widgetSettings);
             }
         }
     });
